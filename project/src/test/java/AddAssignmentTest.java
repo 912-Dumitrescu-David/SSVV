ce357a394f2d4db2b436b11f8bdfb1da817403a2
Dumitrescu-Razvan-alt <razvan.dumitrescu04@gmail.com>
2025-04-11 10:30:39 +0300

    test lab3 in person

---
+++ project/src/test/java/AddAssignmentTest.java
@@ -0,0 +1,47 @@
+import domain.Nota;
+import domain.Student;
+import domain.Tema;
+import org.junit.jupiter.api.Test;
+import repository.NotaXMLRepository;
+import repository.StudentXMLRepository;
+import repository.TemaXMLRepository;
+import service.Service;
+import validation.NotaValidator;
+import validation.StudentValidator;
+import validation.TemaValidator;
+import validation.Validator;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class AddAssignmentTest {
+    Validator<Student> studentValidator = new StudentValidator();
+    Validator<Tema> temaValidator = new TemaValidator();
+    Validator<Nota> notaValidator = new NotaValidator();
+
+    StudentXMLRepository fileRepository1 = new StudentXMLRepository(studentValidator, "studentiTest.xml");
+    TemaXMLRepository fileRepository2 = new TemaXMLRepository(temaValidator, "temeTest.xml");
+    NotaXMLRepository fileRepository3 = new NotaXMLRepository(notaValidator, "noteTest.xml");
+
+    Service service = new Service(fileRepository1, fileRepository2, fileRepository3);
+
+    @Test
+    void saveAssigmentCorrect(){
+        try{
+            assertEquals(0,service.saveTema("2","tema2",9,7));
+        }catch (Exception e){
+            fail("Exception thrown: " + e.getMessage());
+        } finally {
+            service.deleteTema("2");
+        }
+    }
+
+    @Test
+    void saveAssignmentEmptyId(){
+        try{
+            assertEquals(1,service.saveTema("","descriere",7,6));
+        }catch (Exception e){
+            fail("Exception thrown: " + e.getMessage());
+        }
+    }
+}
